a0ef9f07025c5345e15acc908f070b691bc7100b
Charley Cowens <charley.cowens@gmail.com>
2020-06-04 14:07:58 -0700

    Clean up, add URL paths, add excertps as summaries

--- categories-tags-from-wp-posts.R
+++ categories-tags-from-wp-posts.R
@@ -1,62 +1,92 @@
-#make sure there is a designated mirror for running as a script 
+## Take an XML file containing metadata about blog posts that's been organized by 
+## an XSLT script to be easier to load as a dataframe into R. Produce useful
+## information about the blog posts, primarily taxonomic, in an Ecel spreadsheet
+
+# Preliminaries -----------------------------------------------------------
+
+#make sure there is a designated mirror for when this is run as a script 
 local({r <- getOption("repos")
 r["CRAN"] <- "https://cloud.r-project.org/" 
 options(repos=r)
 })
-if(!require(XML)) {install.packages("XML"); library(XML)} #xmlParse() xmlTODataFrame()
+
+#packages required with functions used from those packages
+if(!require(XML)) {install.packages("XML"); library(XML)} #xmlParse() xmlTODataFrame() parseURI()
 if(!require(lubridate)) {install.packages("lubridate"); library(lubridate)} #parse_date_time()
 if(!require(tidyr)) {install.packages("tidyr"); library(tidyr)} #separate_rows
 if(!require(dplyr)) {install.packages("dplyr"); library(dplyr)} #select() group_by() summarise() arrange() mutate()
 if(!require(writexl)) {install.packages("writexl"); library(writexl)} #xl_hyperlink() write_xlsx()
 
+#start
 doc <- xmlParse("sample-wp-posts-info-extract.xml")
 
-#grab the overall table of posts, fixing the date
+# Main worksheet listing all posts sorted by date -------------------------
+
+#grab the overall table of post metadata, fixing the date
 wp_posts <- xmlToDataFrame(nodes = getNodeSet(doc, "//item"), stringsAsFactors=FALSE) %>%
    mutate(date = as_date(parse_date_time(date, "adbYHMSz"))) %>%
-   mutate(title = trimws(title))
+   mutate(title = trimws(title)) 
 
-#create a column for output as linked text in a spreadsheet
+#add a column just showing the path only
+wp_posts$url_path <- sapply(wp_posts$link, function(x) parseURI(x)$path)
+
+#create a column for output as linked text (URL, title) when the spreadsheet is
+#written
 class(wp_posts$link) <- "hyperlink"
 wp_posts$linked_title <- gsub("\"", "\"\"", wp_posts$title) #escape the double quotes for the Excel hypertext formula
 wp_posts$linked_title <- xl_hyperlink(wp_posts$link, name = wp_posts$linked_title)
 
-#order and select the columns for the spreadsheet
-wp_posts <- select(wp_posts, linked_title, date, category, tag)
+#order the columns and sort
+wp_posts <- select(wp_posts, linked_title, summary, date, category, tag, url_path) %>% 
+   arrange(date)
 
+# Posts by Categories worksheet --------------------------------------------
 
-#grab a list of post/category pairs taking into account there can be more than one category for
-#each post which will be a comma-separated list in the category column that can be expanded as multiple rows
-# -- add on boolean column for whether there were mutliple categories
 wp_categories <- wp_posts %>%
    select(category, linked_title) %>%
+   #if there are multiple categories separated by commas for the post flag it
    mutate(multiple = grepl(",",category)) %>% 
+   #if there are multiple categories for the post flag split up into multiple
+   #sharing the same title
    separate_rows(category, sep = ",") %>%
+   #sort by category
    arrange(category)
 
+# Category Count worksheet --------------------------------------------
 
-#summarize by category frequency and sort
 wp_categories_ranked <- wp_categories %>%
    group_by(category) %>%
+   #summarize categories by count like a pivot table
    summarise(category_count=n()) %>%
+   #sort categories by count highest first
    arrange(desc(category_count))
+
+
+# Posts by Tags worksheet --------------------------------------------------
 
 #grab a list of post/tag pairs taking into account there can be more than one tag for
 #each post which will be a comma-separated list in the tag column that can be expanded as multiple rows
 wp_tags <- wp_posts %>%
    select(tag, linked_title) %>%
+   #split up by tags into multiple rows sharing the same title
    separate_rows(tag, sep = ",") %>%
+   #sort by tag used
    arrange(desc(tag))
 
-#summarize by tag frequency and sort
-wp_tags_ranked <- wp_tags %>%
-  group_by(tag) %>%
-  summarise(tag_count=n()) %>%
-  arrange(desc(tag_count))
+# Tag Count worksheet -----------------------------------------------------
 
-#write out an Excel file for everything with specified sheet names
+wp_tags_ranked <- wp_tags %>%
+   group_by(tag) %>%
+   #summarize tags by count like a pivot table
+   summarise(tag_count=n()) %>%
+   #sort tags by count highest first
+   arrange(desc(tag_count))
+
+# Write out an Excel file containing all the sheets -----------------------
+
 write_xlsx(list(Main = wp_posts,
                 `Posts by Categories` = wp_categories,
                 `Category Count` = wp_categories_ranked,
                 `Posts by Tags` = wp_tags,
                 `Tag Count` = wp_tags_ranked), path="sample-posts-analyzed.xlsx")
+
