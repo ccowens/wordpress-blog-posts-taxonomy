19b18322e115d773ff0282496470787555fa0d83
Charley Cowens <charley.cowens@gmail.com>
2020-09-14 22:57:17 -0700

    just new version of R

--- example-script.bat
+++ example-script.bat
@@ -1,3 +1,3 @@
 java -cp "C:\Program Files\Saxon\saxon9he.jar"  net.sf.saxon.Transform -t -s:exported-from-wp.xml -xsl:flatten-wp-xml.xslt -o:extracted-info.xml
-"c:\Program Files\R\R-4.0.0\bin\RScript" categories-tags-from-wp-posts.R
+"c:\Program Files\R\R-4.0.2\bin\RScript" categories-tags-from-wp-posts.R
 pause
